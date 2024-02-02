ebdefe72de55492b8e9dd01a0d981ecddc8d6157
Ever Henry Abanto Vilchez <eabantov20_1@unc.edu.pe>
2024-02-01 22:48:08 -0500

    Actualizacion de los enlaces del Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => {
            <div className='footer__section'>
            <b >
                 <a className='s' href="https://maps.app.goo.gl/3ykT51E4KRyd9J5H7" target="_blank" rel="noopener noreferrer">
-                    <i className="fas fa-map-marker-alt"></i> Jr. Soledad S/N, Costado Del Coliseo De Namora
+
+                    <i className="fas fa-map-marker-alt"></i> AV. Los heroes 0434 Bar. San Sebastian, Cajamarca
                 </a>
             </b>
            </div>
@@ -26,11 +27,11 @@ const Footer = () => {
 
             <div className='footer__section'>
                 <b>
-                    <a href="tel:+51930912572"><i className="fas fa-phone"></i> 930 912 572</a>
+                    <a href="tel:+51930702459"><i className="fas fa-phone"></i> 930 702 459</a>
 
                 </b>
                 <b>
-                    <a href="https://api.whatsapp.com/send?phone=51930912572" target="_blank" rel="noopener noreferrer"> <i className="fab fa-whatsapp"></i> 930 912 572</a>
+                    <a href="https://api.whatsapp.com/send?phone=51930702459" target="_blank" rel="noopener noreferrer"> <i className="fab fa-whatsapp"></i> 930 702 459</a>
                 </b>
             </div>
         </div>
