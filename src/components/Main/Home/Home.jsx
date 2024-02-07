56702fac13af33170c1d5a2fed15ce4086b16cde
AntonyReyesFigueroa <antonyreyesfigueroa@gmail.com>
2024-02-06 19:49:16 -0500

    subiendo el segundo nav

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
-import ComprarTodo from './pages/ComprarTodo/ComprarTodo'
 import './Home.css'
+import ComprarTodo from './pages/ComprarTodo/ComprarTodo'
 import { useState } from 'react'
 import Celulares from './pages/Celulares/Celulares'
 import Audifonos from './pages/Audifonos/Audifonos'
 import Ofertas from './pages/Ofertas/Ofertas'
 const Home = () => {
 
-  const [headerLink, setHeaderLink] = useState('celulares');
+  const [headerLink, setHeaderLink] = useState('comprarTodo');
+
+
+  
+  const [lapis, setLapis] = useState()
 
   const headerComprarTodo = () => {
     setHeaderLink('comprarTodo')
@@ -31,21 +34,39 @@ const Home = () => {
   return (
     <div>
 
-      <div className='container_header_home'>
+      {/* <div className='container_header_home'>
         <ul className='ul_header_home nav nav-tabs d-flex justify-content-start'>
           <li onClick={ () => setHeaderLink('comprarTodo')} className='li_header_home nav-item'><a className=' nav-link' href="">Comprar todo</a></li>
           <li onClick={headerCelulares} className='li_header_home nav-item'><a className=' nav-link' href="">Celulares</a></li>
           <li onClick={headerAudifonos} className='li_header_home nav-item'><a className=' nav-link' href="">Audifonos</a></li>
           <li onClick={headerOfertas} className='li_header_home nav-item'><a className=' nav-link' href="">Ofertas</a></li>
         </ul>
+      </div> */}
+
+      <div className='nav_header_home'>
+        <ul className='ul_header_home'>
+          <li onClick={headerComprarTodo} className='li_header_home'>Comprar todo</li>
+          <li onClick={headerCelulares} className='li_header_home'>Celulares</li>
+          <li onClick={headerAudifonos} className='li_header_home'>Audifonos</li>
+          <li onClick={headerOfertas} className='li_header_home'>Ofertas</li>
+        </ul>
       </div>
 
 
 
-      <div className='p-5 mt-5' >
+      <div className='container_pages' >
         {
           headerLink==="comprarTodo" ?
             <ComprarTodo />
+            : 
+            headerLink==="celulares" ?
+            <Celulares />
+            : 
+            headerLink==="audifonos" ?
+            <Audifonos />
+            : 
+            headerLink==="ofertas" ?
+            <Ofertas />
             : 
             ''
         }
--- src/components/Main/Home/pages/ComprarTodo/ComprarTodo.jsx
+++ src/components/Main/Home/pages/ComprarTodo/ComprarTodo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './ComprarTodo.css'
 
 const ComprarTodo = () => {
   return (
-    <div>
+    <div className='container_ct'>
 
       <h1>Estamos en comprar todo</h1>
 
